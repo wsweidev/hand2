9f8771c4e85f8adcc89bfb7ee6cc5fa80b78f61b
wsweidev <107857037+wsweidev@users.noreply.github.com>
2022-07-14 04:07:47 +0800

    fix chat routing

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -18,16 +18,19 @@ import {
     Heading,
     HStack,
     useToast,
+    Center,
 } from "@chakra-ui/react";
 import { trpc } from "@src/utils/trpc";
 import { ChangeEvent, useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import dayjs from "dayjs";
 import { AutoResizeTextarea } from "@src/components/GenericUIComponents/AutoResizeTextarea";
+import { useRouter } from "next/router";
 
 const Chat: NextPage = () => {
     const session = useSession();
     const toast = useToast();
+    const router = useRouter();
     const getChatsQuery = trpc.useQuery(["chats.getUserChats"], {
         refetchInterval: 5000,
     });
@@ -73,66 +76,75 @@ const Chat: NextPage = () => {
 
     return (
         <>
+            {getChatsQuery.isLoading && (
+                <Center fontWeight={"semibold"} fontSize={"2xl"} h="400px">
+                    {"Loading..."}
+                </Center>
+            )}
             <Flex direction={["column", "column", "row"]} h="80vh">
-                <Box
-                    borderRadius="15px"
-                    bg={"gray.200"}
-                    _dark={{ bg: "gray.700" }}
-                    alignSelf="center"
-                >
-                    <Stack
-                        overflow={"auto"}
-                        p={"15px"}
-                        spacing={"10px"}
-                        w={["80vw", "80vw", "fit-content"]}
-                        h={["fit-content", "fit-content", "80vh"]}
+                {!!chats && (
+                    <Box
                         borderRadius="15px"
-                        direction={["row", "row", "column"]}
-                        sx={{
-                            "&::-webkit-scrollbar": {
-                                width: "16px",
-                                borderRadius: "8px",
-                                backgroundColor: `rgba(0, 0, 0, 0.1)`,
-                            },
-                            "&::-webkit-scrollbar-thumb": {
-                                backgroundColor: `rgba(0, 0, 0, 0.05)`,
-                            },
-                        }}
+                        bg={"gray.200"}
+                        _dark={{ bg: "gray.700" }}
+                        alignSelf="center"
                     >
-                        {!!chats &&
-                            !!chatUsers &&
-                            chatUsers.map((userName) => {
-                                const otherUser =
-                                    chats[userName]?.[0]?.receiverId ===
-                                    session.data?.user?.id
-                                        ? chats[userName]?.[0]?.sender
-                                        : chats[userName]?.[0]?.receiver;
-                                return (
-                                    <Box key={userName}>
-                                        <Button
-                                            onClick={() => {
-                                                setSelectedUser(userName);
-                                                setReceiverId(otherUser?.id);
-                                            }}
-                                            alignItems="center"
-                                            colorScheme={
-                                                userName === selectedUser
-                                                    ? "teal"
-                                                    : undefined
-                                            }
-                                        >
-                                            <Avatar
-                                                size={"xs"}
-                                                mr="5px"
-                                                src={otherUser?.image ?? ""}
-                                            />
-                                            {userName}
-                                        </Button>
-                                    </Box>
-                                );
-                            })}
-                    </Stack>
-                </Box>
+                        <Stack
+                            overflow={"auto"}
+                            p={"15px"}
+                            spacing={"10px"}
+                            w={["80vw", "80vw", "fit-content"]}
+                            h={["fit-content", "fit-content", "80vh"]}
+                            borderRadius="15px"
+                            direction={["row", "row", "column"]}
+                            sx={{
+                                "&::-webkit-scrollbar": {
+                                    width: "16px",
+                                    borderRadius: "8px",
+                                    backgroundColor: `rgba(0, 0, 0, 0.1)`,
+                                },
+                                "&::-webkit-scrollbar-thumb": {
+                                    backgroundColor: `rgba(0, 0, 0, 0.05)`,
+                                },
+                            }}
+                        >
+                            {!!chats &&
+                                !!chatUsers &&
+                                chatUsers.map((userName) => {
+                                    const otherUser =
+                                        chats[userName]?.[0]?.receiverId ===
+                                        session.data?.user?.id
+                                            ? chats[userName]?.[0]?.sender
+                                            : chats[userName]?.[0]?.receiver;
+                                    return (
+                                        <Box key={userName}>
+                                            <Button
+                                                onClick={() => {
+                                                    setSelectedUser(userName);
+                                                    setReceiverId(
+                                                        otherUser?.id
+                                                    );
+                                                }}
+                                                alignItems="center"
+                                                colorScheme={
+                                                    userName === selectedUser
+                                                        ? "teal"
+                                                        : undefined
+                                                }
+                                            >
+                                                <Avatar
+                                                    size={"xs"}
+                                                    mr="5px"
+                                                    src={otherUser?.image ?? ""}
+                                                />
+                                                {userName}
+                                            </Button>
+                                        </Box>
+                                    );
+                                })}
+                        </Stack>
+                    </Box>
+                )}
 
                 <Box
                     w={"100%"}
@@ -201,7 +213,21 @@ const Chat: NextPage = () => {
                                                                         .mainImageUrl!
                                                                 }
                                                             />
-                                                            <Text ml="5px">
+                                                            <Text
+                                                                ml="5px"
+                                                                cursor={
+                                                                    "pointer"
+                                                                }
+                                                                _hover={{
+                                                                    fontWeight:
+                                                                        "semibold",
+                                                                }}
+                                                                onClick={() => {
+                                                                    router.push(
+                                                                        `/Listing/${message?.mentionedListing?.id}`
+                                                                    );
+                                                                }}
+                                                            >
                                                                 {
                                                                     message
                                                                         .mentionedListing
@@ -260,7 +286,21 @@ const Chat: NextPage = () => {
                                                                         .mainImageUrl!
                                                                 }
                                                             />
-                                                            <Text ml="5px">
+                                                            <Text
+                                                                ml="5px"
+                                                                cursor={
+                                                                    "pointer"
+                                                                }
+                                                                _hover={{
+                                                                    fontWeight:
+                                                                        "semibold",
+                                                                }}
+                                                                onClick={() => {
+                                                                    router.push(
+                                                                        `/Listing/${message?.mentionedListing?.id}`
+                                                                    );
+                                                                }}
+                                                            >
                                                                 {
                                                                     message
                                                                         .mentionedListing
